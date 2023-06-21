0af40729a29078bfb7c1a9dbeeb8379fb7dc715f
Michał Murawski <m.a.murawski@wp.pl>
2023-06-21 08:42:46 +0200

    add presentation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import { SwitchDayNight1 } from './components/SwitchDayNight1';
 import { SwitchDayNight2 } from './components/SwitchDayNight2';
 
 function App() {
+  const [switches, setSwitches] = useState({
+    switch1: 'light',
+    switch2: 'dark',
+  });
+
   const handleClick1 = mode => {
-    console.log('mode 1:', mode);
+    setSwitches(previous => ({ ...previous, switch1: mode }));
   };
 
   const handleClick2 = mode => {
-    console.log('mode 2:', mode);
+    setSwitches(previous => ({ ...previous, switch2: mode }));
   };
 
   return (
@@ -21,11 +27,39 @@ function App() {
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'lightgray',
-        gap: '30px',
+        gap: '100px',
       }}
     >
-      <SwitchDayNight1 scale="4" onClick={handleClick1} />
-      <SwitchDayNight2 scale="4" onClick={handleClick2} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '15px',
+        }}
+      >
+        <SwitchDayNight1
+          scale="4"
+          mode={switches.switch1}
+          onClick={handleClick1}
+        />
+        <p style={{ fontSize: '30px' }}>Switch 1 - {switches.switch1} mode</p>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '15px',
+        }}
+      >
+        <SwitchDayNight2
+          scale="4"
+          mode={switches.switch2}
+          onClick={handleClick2}
+        />
+        <p style={{ fontSize: '30px' }}>Switch 2 - {switches.switch2} mode</p>
+      </div>
     </div>
   );
 }
